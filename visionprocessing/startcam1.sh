5861583d36db299bca29d17bcfb4d92527fe599b
Sithija Gunasinghe <sparks10.sg@gmail.com>
2017-02-03 17:25:21 -0600

    changed camera settings so they actually work

--- visionprocessing/startcam1.sh
+++ visionprocessing/startcam1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 v4l2-ctl --device 1 \
  --set-ctrl brightness=0 \
- --set-ctrl contrast=0 \
+ --set-ctrl contrast=255 \
  --set-ctrl saturation=255 \
  --set-ctrl gain=0 \
  --set-ctrl sharpness=0 \
