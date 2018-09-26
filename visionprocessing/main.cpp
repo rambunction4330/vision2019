e3a52be9218620458e8a6395249a8425c3a7dd67
Alex Krasnoff <Alex@Dales-MacBook-Air.local>
2018-09-26 15:54:44 -0500

    deleted useless libraries

--- visionprocessing/main.cpp
+++ visionprocessing/main.cpp
@@ -13,10 +13,6 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/core.hpp"
-// #include "opencv2/gpu/gpu.hpp"
-// #include "opencv2/gpu/gpumat.hpp"
-//#include "opencv2/cudaimgproc.hpp"
-//#include "opencv2/core/cuda.hpp"
 #include "opencv2/cudaarithm.hpp"
 
 #define PORTNUMBER  9001 
